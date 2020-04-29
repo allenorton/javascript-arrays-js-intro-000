e91c6118ea49726a108c8cab105accf510ccddb0
Elizabeth Allene Norton <norton.allene@gmail.com>
2020-04-29 22:18:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,6 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 function removeElementFromBeginningOfArray(array){
   array.slice(1)
+  return array
 }
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
