3309ca623ddbf14c7bce469ce372e81e58208d7f
Elizabeth Allene Norton <norton.allene@gmail.com>
2020-04-29 22:04:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,5 +9,6 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
-  return array.unshift(element)
+  array.unshift(element)
+  return array
 }
